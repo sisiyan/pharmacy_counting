07bf99de4007ab31e0c7e525f4209873224eb6c4
Si Yan <syan62@gatech.edu>
2018-07-27 11:06:36 -0400

    more robust

--- src/Pharmacy_Counting.java
+++ src/Pharmacy_Counting.java
@@ -20,41 +20,46 @@ public class Pharmacy_Counting {
             System.out.println("Please give an input file!");
             return;
         }
-        
-        
+
+
         long startTime = System.nanoTime();
         HashMap<String, HashSet<String>> map_dp = new HashMap<>(); //map to store <drug name, prescriber name list for that drug>
         HashMap<String, Float> map_dc = new HashMap<>(); //map to store <drug name, drug total cost>
         Map<Integer, ArrayList<String>> map = new TreeMap<>(Collections.reverseOrder()); //treemap to keep the processed record in order of total cost
-        
-        
+
+
         //Process the drug name start with each character.
         for(char ch = 'A'; ch <= 'Z'; ch++) {
-             
+
             try (Scanner sc = new Scanner(new File(args[0]), "UTF-8")) {
-                
-                long freeMemoryBefore = Runtime.getRuntime().freeMemory();
+
+                //long freeMemoryBefore = Runtime.getRuntime().freeMemory();
                 //read in records
-                sc.nextLine();
-                while (sc.hasNextLine()) {                    
+                sc.nextLine(); //skip the first line (column names)
+                while (sc.hasNextLine()) {
                     String line = sc.nextLine();
-                    String[] tokens = line.split(",");
-                    if(tokens.length != 5) continue;
-                     
-                    if(tokens[3].charAt(0) == ch) {
+                    String[] tokens = line.split(",");  //Read each line convert string to a string array
+                    if(tokens.length != 5) continue; //Skip the bad inputs.
+                    //It is safer add a line of code to check that the last column is number
+                    if(tokens[4] != null && !tokens[4].matches("\\d*\\.?\\d+")) continue;
+
+                    if(tokens[3].charAt(0) == ch) { //Process the drug name start with each character.
                         String fullname = tokens[1] + "_" + tokens[2];
-                         
-                        if(!map_dc.containsKey(tokens[3])) map_dc.put(tokens[3], Float.parseFloat(tokens[4]));
-                        else {
-                            map_dc.replace(tokens[3], map_dc.get(tokens[3]) + Float.parseFloat(tokens[4]));
-                        }
-                         
+
+
+                        //Add a new entry in the map if drug name not in the map yet, otherwise update the set of prescriber names
                         if(!map_dp.containsKey(tokens[3])) {
                             map_dp.put(tokens[3], new HashSet<String>());
                             map_dp.get(tokens[3]).add(fullname);
                         }
                         else {
                             map_dp.get(tokens[3]).add(fullname);
+                        }
+
+                        //Add a new entry if drug name not already in the map, otherwise update the total drug cost.
+                        if(!map_dc.containsKey(tokens[3])) map_dc.put(tokens[3], Float.parseFloat(tokens[4]));
+                        else {
+                            map_dc.replace(tokens[3], map_dc.get(tokens[3]) + Float.parseFloat(tokens[4]));
                         }
                     }
                  }
@@ -67,40 +72,42 @@ public class Pharmacy_Counting {
              catch (FileNotFoundException e) {
                  e.printStackTrace();
              }
-             
+
+             //Construct the output lines with drug name, number of prescribers, and total cost
+             //put the <totalCost, List<lines>> into the treeMap
              for(String drug: map_dp.keySet()) {
                  int totalCost = Math.round(map_dc.get(drug));
-                 String record = drug + ","+ map_dp.get(drug).size() +"," + totalCost;
+                 String record = drug + ","+ map_dp.get(drug).size() +"," + totalCost; //Construct the output lines
                  if(!map.containsKey(totalCost)) {
                      map.put(totalCost, new ArrayList<String>());
                      map.get(totalCost).add(record);
                  }
-                 else map.get(totalCost).add(record);    
-                 
+                 else map.get(totalCost).add(record);
+
              }
              map_dp.clear();
              map_dc.clear();
         }
-        
+
         try (Scanner sc = new Scanner(new File(args[0]), "UTF-8")) {
-            long freeMemoryBefore = Runtime.getRuntime().freeMemory();
-            
+            //long freeMemoryBefore = Runtime.getRuntime().freeMemory();
+
             sc.nextLine();
             while (sc.hasNextLine()) {
                 String line = sc.nextLine();
-                
+
                 String[] tokens = line.split(",");
                 if(tokens.length != 5) continue;
-                     
+
                 if(tokens[3].charAt(0) < 'A' || tokens[3].charAt(0) > 'Z') {
-                    
+
                     String fullname = tokens[1] + "_" + tokens[2];
-                         
+
                      if(!map_dc.containsKey(tokens[3])) map_dc.put(tokens[3], Float.parseFloat(tokens[4]));
                      else {
                          map_dc.replace(tokens[3], map_dc.get(tokens[3]) + Float.parseFloat(tokens[4]));
                      }
-                         
+
                      if(!map_dp.containsKey(tokens[3])) {
                          map_dp.put(tokens[3], new HashSet<String>());
                          map_dp.get(tokens[3]).add(fullname);
@@ -117,26 +124,26 @@ public class Pharmacy_Counting {
          catch (FileNotFoundException e) {
              e.printStackTrace();
          }
-         
+
          for(String drug: map_dp.keySet()) {
-             
+
              int totalCost = Math.round(map_dc.get(drug));
              String record = drug + ","+ map_dp.get(drug).size() +"," + totalCost;
              if(!map.containsKey(totalCost)) {
                   map.put(totalCost, new ArrayList<String>());
                   map.get(totalCost).add(record);
              }
-             else map.get(totalCost).add(record);    
-                 
+             else map.get(totalCost).add(record);
+
          }
          map_dp.clear();
          map_dc.clear();
-         
+
          if(map.size() == 0) {
-            System.out.println("Input file is empty, or All input records are illegally formatted!"); 
+            System.out.println("Input file is empty, or All input records are illegally formatted!");
             return;
          }
-         
+
          String outputFileName;
          if(args.length == 2) {
              File tempFile = new File(args[1]);
@@ -154,31 +161,31 @@ public class Pharmacy_Counting {
              //outputFile = new File("./output/output.txt");
              outputFileName = "./output/output.txt";
          }
-         
-         try {   
+
+         try {
              BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName));
              writer.write("drug_name,num_prescriber,total_cost\n");
-             for(List<String> val : map.values()){ 
+             for(List<String> val : map.values()){
                  if(val.size() > 1) {
                      Collections.sort(val);
                      Collections.reverse(val);
                      for(String record: val) {
                          writer.write(record);
                          writer.newLine();
-                     }                    
+                     }
                  }
                  else {
                      writer.write(val.get(0));
                      writer.newLine();
                  }
-                 
+
              }
              writer.close();
          }
          catch (IOException e1) {
              e1.printStackTrace();
          }
-         
+
          long endTime = System.nanoTime();
          long elapsedTimeInMillis = TimeUnit.MILLISECONDS.convert((endTime - startTime), TimeUnit.NANOSECONDS);
          System.out.println("Total elapsed time: " + elapsedTimeInMillis + " ms"  );
